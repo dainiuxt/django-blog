961e4173112af137f259e712d3aec3d06e9aa41e
dainiuxt <dainiuxt@gmail.com>
2022-05-10 18:59:17 +0300

    add models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,17 @@
+from ast import mod
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from datetime import date
+from tinymce.models import HTMLField
 
-# Create your models here.
+class BlogPost(models.Model):
+  post_title = models.CharField('Post tile', max_length=500, null=True)
+  post_author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+  pub_time = models.DateTimeField('Publication time', null=True, blank=True)
+  post_text = HTMLField()
+
+class Comment(models.Model):
+  comment_author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+  pub_time = models.DateTimeField('Publication time', null=True, blank=True)
+  comment_text = models.TextField('Comment')
