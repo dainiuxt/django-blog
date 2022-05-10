bbbcd0ade7765d7f3e7ea8d6a24fb989879d0d76
dainiuxt <dainiuxt@gmail.com>
2022-05-10 20:21:05 +0300

    add postlist and pagination to home page

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, reverse, redirect
 from blog.models import BlogPost, Comment
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
-    all_posts = BlogPost.objects.all()
-    context = {
-      'all_posts': all_posts,
-    }
-    return render(request, 'index.html', context=context)
+    post_list = BlogPost.objects.all()
+    page = request.GET.get('page', 1)
+    # context = {
+    #   'post_list': post_list,
+    # }
+    paginator = Paginator(post_list, 10)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'index.html', {'posts': posts})
