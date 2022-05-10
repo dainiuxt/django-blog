91086ed9a91e8be5342a4c1f9702801114ad6a37
dainiuxt <dainiuxt@gmail.com>
2022-05-10 19:07:42 +0300

    add meta to models and link comments with posts

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -11,7 +11,24 @@ class BlogPost(models.Model):
   pub_time = models.DateTimeField('Publication time', null=True, blank=True)
   post_text = HTMLField()
 
+  def __str__(self):
+      return f'{self.post_title} by {self.post_author}'
+
+  class Meta:
+      verbose_name = 'Post'
+      verbose_name_plural = 'Posts'
+
+
 class Comment(models.Model):
+  comment_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, null=True, blank=True)
   comment_author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
   pub_time = models.DateTimeField('Publication time', null=True, blank=True)
   comment_text = models.TextField('Comment')
+
+  def __str__(self):
+      return f'{self.pub_time} by {self.comment_author}'
+
+  class Meta:
+      verbose_name = 'Comment'
+      verbose_name_plural = 'Comments'
+
